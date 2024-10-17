300b8852c8d5339e561d865dbcedc61ed415100e
KauaneRibeir <dasilvaribeirokauane@gmail.com>
2024-10-17 19:34:54 -0300

    ok

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 function dbConfig() {
     mongoose
-    .connect(`mongodb+srv://dasilvaribeirokauane:<db_password>@cluster0.8pzqv.mongodb.net/`)
+    .connect(`mongodb+srv://dasilvaribeirokauane:<W3eacObibOw7HTNM>@cluster0.8pzqv.mongodb.net/`)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 }
